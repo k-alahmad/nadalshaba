d1d6abd11ce72c4b3ed8c2b803f975670a96245a
moazabdulhalim <moazabdulhalim@gmail.com>
2023-10-17 12:56:21 +0300

    fix error in sliding text

--- src/pages/home/components/Header/HeaderT1/HeaderSlidingText.jsx
+++ src/pages/home/components/Header/HeaderT1/HeaderSlidingText.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { data } from "../../../../../data/headerData";
 import { systemSettings } from "../../../../../settings";
 import { useLocation } from "react-router-dom";
 const HeaderSlidingText = () => {
   const location = useLocation();
-  let title =
-    systemSettings.availableLanguages.length > 1
-      ? data.titles[2].find(
-          (title) => title.lng == location.pathname.substring(1)
-        )?.value
-      : data.titles[2][0];
+  const [title, setTitle] = useState([]);
 
-  let headerTitle = title.split(" ");
+  useEffect(() => {
+    let tempTitle =
+      systemSettings.availableLanguages.length > 1
+        ? data.titles[2].find(
+            (title) => title.lng == location.pathname.substring(1)
+          )?.value
+        : data.titles[2][0].value;
+
+    let headerTitle = tempTitle.split(" ");
+
+    setTitle(headerTitle);
+
+    console.log(headerTitle);
+  }, []);
+
   return (
     <div className="text-9xl bg-transparent text-primary overflow-hidden  -mt-[350px] max-md:hidden">
       <p className="animate-textCamera relative whitespace-nowrap py-4">
-        {headerTitle[0]}
-        <span className="border-text text-transparent">
-          {` ${headerTitle[1]} `}
-        </span>
-        {headerTitle[2]}
+        {title[0]}
+        <span className="border-text text-transparent">{` ${title[1]} `}</span>
+        {title[2]}
       </p>
     </div>
   );
